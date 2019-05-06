a22717bbc893f6d882cd5d964b4c3de6542c1693
hajnalandor <hajnal.andor1993@gmail.com>
2019-05-07 00:32:28 +0200

    addig gameLoop

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,49 @@ public class Main {
         Board board=Board.getBoard(9);
         Player player1= Player.getPlayerInstance();
         Player player2 = Player.getPlayerInstance();
-        Player player3 = Player.getPlayerInstance();
-      /*  board.printBoard();
-        System.out.println(board.getActiveChainCollection().size());
-        board.addStone(5,5,player1);
-        board.addStone(5,4,player1);
-        System.out.println(board.getActiveChainCollection().size());
-        board.printBoard();
-        Player player2 = new Player(Player.side.WHITE);
-        board.addStone(5,3,player2);
-        board.addStone(5,6,player2);
-        board.printBoard();
-        System.out.println("chaincollection: " + board.getActiveChainCollection().size());
-        board.addStone(4,5,player2);
-        board.addStone(6,5,player2);
-        board.printBoard();*/
       Scanner sc= new Scanner(System.in);
         board.printBoard();
-
+        int[] checkCounter=new int[]{0,0,0};
+        String[] t1;
+        int x;
+        int y;
         while(true) {
             System.out.println(player1.getColor().toString()+ "  lepese: ");
           String t=sc.nextLine();
-          String[] t1=t.split(" ");
-          int x=Integer.parseInt(t1[0]);
-          int y=Integer.parseInt(t1[1]);
-          board.addStone(y,x,player1);
-          board.printBoard();
+          if (t.equalsIgnoreCase("passz")) {
+              checkCounter[0]++;
+              checkCounter[2]++;
+              if (checkCounter[2]==2) {
+                  System.out.println("Jatek vege");
+                  System.exit(0);
+              }
+          } else {
+              t1 = t.split(" ");
+              x = Integer.parseInt(t1[0]);
+              y = Integer.parseInt(t1[1]);
+              board.addStone(y, x, player1);
+              board.printBoard();
+              checkCounter[2]=0;
+          }
           System.out.println(" ");
             System.out.println(player2.getColor().toString()+ "  lepese: ");
+            t = sc.nextLine();
+            if (t.equalsIgnoreCase("passz")) {
+                checkCounter[1]++;
+                checkCounter[2]++;
+                if (checkCounter[2]==2) {
+                    System.out.println("Jatek vege");
+                    System.exit(0);
+                }
+            } else {
+                t1 = t.split(" ");
+                x = Integer.parseInt(t1[0]);
+                y = Integer.parseInt(t1[1]);
+                board.addStone(y, x, player2);
+                board.printBoard();
+                checkCounter[2]=0;
 
-            t=sc.nextLine();
-          t1=t.split(" ");
-          x=Integer.parseInt(t1[0]);
-          y=Integer.parseInt(t1[1]);
-          board.addStone(y,x,player2);
-          board.printBoard();
+            }
 
       }
     }
